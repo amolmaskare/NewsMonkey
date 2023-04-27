333ff25e7dae36d0a04593e7e86d88f63149e949
Amol Sartajsingh Maskare <amolmaskare1@gmail.com>
2023-04-27 18:57:28 +0530

    first commit

---
+++ src/components/NewApi.js
@@ -0,0 +1,16 @@
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://api.newscatcherapi.com/v2/search',
+//   params: {q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1'},
+//   headers: {
+//     'x-api-key': 'XPpr7o1IAgbAY5wPqDLTuYUqCPncuUC1kAaVx8HYdCs'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
